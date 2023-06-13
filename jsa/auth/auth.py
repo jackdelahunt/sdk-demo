14b6ad9d0e6a3eb4c45976d2ffa227512dd44536
Jack Delahunt <45426048+jackdelahunt@users.noreply.github.com>
2023-06-13 12:12:22 +0100

    fix: keycloak

--- jsa/auth/auth.py
+++ jsa/auth/auth.py
@@ -4,13 +4,16 @@ from kiota_abstractions.authentication.access_token_provider import (
 from kiota_abstractions.authentication.allowed_hosts_validator import AllowedHostsValidator
 import os
 import requests
+from keycloak import KeycloakOpenID
+
+DEFAULT_AUTH_URL  = "https://sso.redhat.com/auth/"
+DEFAULT_CLIENT_ID = "cloud-services"
+REALM_NAME = "redhat-external"
 
 class JSAAccessTokenProvider(AccessTokenProvider):
     def __init__(self):
         self.offline_token = os.environ['OFFLINE_TOKEN']
         self.allowed_hosts_validator = AllowedHostsValidator(["sso.redhat.com", "api.openshift.com"])
-        self.client_id = "cloud-services"
-        self.url = "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token"
 
     async def get_authorization_token(self, uri: str) -> str:
         return self.new_token()
@@ -19,27 +22,12 @@ class JSAAccessTokenProvider(AccessTokenProvider):
         return self.allowed_hosts_validator
     
     def new_token(self) -> str:
-        data = {
-            "grant_type": "refresh_token",
-            "client_id": self.client_id,
-            "refresh_token": self.offline_token
-        }
-
-        print(data)
-
-        response = requests.post(self.url, data=data)
-
-        print(f"RESPONSE :: \n {response}")
-
-        code = response.status_code
-        if code == 200:
-            body = response.json()
-            try:
-                token = body["access_token"]
-            except Exception as e:
-                raise RuntimeError("Error issuing a new token, received answer with body " + str(body)) from e
-        else:
-            raise RuntimeError("Error issuing a new token, received answer code " + str(code))
-
-        return token
-
+        # Configure keycloak_openid client 
+        keycloak_openid = KeycloakOpenID(server_url=DEFAULT_AUTH_URL,
+            client_id=DEFAULT_CLIENT_ID,
+            realm_name=REALM_NAME)
+        
+        # Refresh token
+        token = keycloak_openid.refresh_token(self.offline_token)
+        # print(f"REFRESH TOKEN :: {token}")
+        return token["access_token"]
